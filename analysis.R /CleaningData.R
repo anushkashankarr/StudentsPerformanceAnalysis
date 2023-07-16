9fb06af9670695a5881445a2cf8a2c807f997a64
Anushka Shankar <116955584+anushkashankarr@users.noreply.github.com>
2023-07-16 21:27:41 +0530

    Update CleaningData.R

--- analysis.R /CleaningData.R
+++ analysis.R /CleaningData.R
@@ -1,3 +1,9 @@
+
+
+#------------------------------------------------------------------- Data cleaning and manipulation-------------------------------------------------------
+
+
+# making the column names handy.
 student <- student %>% rename(parental_education = parental.level.of.education,
                               math_score = math.score,
                               writing_score = writing.score,
@@ -6,5 +12,44 @@ student <- student %>% rename(parental_education = parental.level.of.education,
                               test_prep_course = test.preparation.course)
 
 
+
+# Missing values and duplicates
+sum(is.na(student))
+duplicates <- student %>% duplicated()
+duplicates_count <- table(duplicates)
+duplicates_count
+
+
 student_cleaned <- student %>%
-  filter(!is.na(cgpa) & !is.na(package))
+  rename(package_score = package, cgpa_score = cgpa) %>%
+  mutate(across(where(is.character), as.factor))
+
+# Fill missing values with mean
+numeric_columns <- c("math.score", "reading.score", "writing.score")
+categorical_columns <- c("parental.level.of.education", "lunch", "test.preparation.course", "gender", "race.ethnicity")
+
+for (col in numeric_columns) {
+  student_cleaned[[col]] <- ifelse(is.na(student_cleaned[[col]]), mean(student_cleaned[[col]], na.rm = TRUE), student_cleaned[[col]])
+}
+
+for (col in categorical_columns) {
+  student_cleaned[[col]] <- ifelse(is.na(student_cleaned[[col]]), as.character(mode(student_cleaned[[col]], na.rm = TRUE)), student_cleaned[[col]])
+}
+
+
+# Verify missing values have been filled
+sum(is.na(student_cleaned))
+
+
+# Updated dataset with cleaned data
+head(student_cleaned)
+
+
+# Data cleaning and manipulation of cgpa and package column
+student_cleaned <- student %>%
+  rename(package_score = package, cgpa_score = cgpa) %>%
+  mutate(across(where(is.character), as.factor)) %>%
+  mutate(across(everything(), ~ifelse(is.na(.), mean(., na.rm = TRUE), .)))
+
+
+
